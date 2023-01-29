363a7414a868f5273eb888715af9da62ae8902eb
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-01-29 15:41:53 +0900

    11
    11

--- 10819 차이를 최대로_순열.py
+++ 10819 차이를 최대로_순열.py
@@ -1,18 +1,19 @@
+#순열로 푸는법.
 from itertools import permutations
 
 n = int(input())
 arr = list(map(int ,input().split()))
 
+#모든 순열을 다 리스트에 넣는다. 
 permu = list(permutations(arr, n))
-print(permu)
-# def calculator(li):
-#   total = 0
-#   for i in range(len(li)-1):
-#     total += abs(li[i]-li[i+1])
-#   return total
+# print(permu)
+def calculator(li):
+  total = 0
+  for i in range(len(li)-1):
+    total += abs(li[i]-li[i+1])
+  return total
 
-# answer = -1e9
-# for li in permu:
-#   answer = max(answer, calculator(li))
-
-# print(answer)
+answer = -1e9
+for li in permu:
+  answer = max(answer, calculator(li))
+print(answer)
--- 10819 차이를 최대로_재귀.py
+++ 10819 차이를 최대로_재귀.py
@@ -3,15 +3,18 @@ n = int(input())
 in_list = list(map(int ,sys.stdin.readline().split()))
 visited = [False]*n
 ansArr = []
+
 def sol(my_list):
     #모든 값이 my_list에 들어왔으면
   if len(my_list) == n:
     total = 0
+    # 리스트의 모든 값 계산하기
     for i in range(n-1):
       total += abs(my_list[i]- my_list[i+1])
+    #계산 값 저장하기
     ansArr.append(total)
     return
-
+    #재귀 시작
   for i in range(n):
     if not visited[i]:
       visited[i] = True
