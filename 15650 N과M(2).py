08c2e5eb4edde8d77111a1343db2a67d51f8a466
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-01-30 01:22:00 +0900

    11
    11

--- 15650 N과M(2).py
+++ 15650 N과M(2).py
@@ -1,4 +1,5 @@
 import sys
+<<<<<<< HEAD
 
 def getSubsum(data) :
     '''
@@ -13,6 +14,21 @@ def getSubsum(data) :
             continue
         res = max(res,sum)
     return res
+=======
+read = sys.stdin.readline
+global N, M, visited
+def comb(cnt, start):
+    if cnt == M:
+        for i in range(1, N + 1):
+            if visited[i]:
+                print(i, end=' ')
+        print()
+        return
+    for i in range(start, N + 1):
+        visited[i] = True
+        comb(cnt + 1, i + 1)
+        visited[i] = False
+>>>>>>> 363a7414a868f5273eb888715af9da62ae8902eb
 
 def main():
     '''
--- 7568 덩치.py
+++ 7568 덩치.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #그냥 o(n^2) 때리기 
 
 n = int(input())
@@ -17,3 +18,38 @@ for i in range(n):
  
 for d in ans:
     print(d,end=" ")
+=======
+'''
+5
+55 185
+58 183
+88 186
+60 175
+46 155
+'''
+import sys
+n = int(sys.stdin.readline())
+my_list = []
+for i in range(n):
+    my_list.append(tuple(map(int,sys.stdin.readline().split())))
+# print(n)
+# print(my_list)
+
+my_rank_list = []
+my_rank = 1
+for i,j in enumerate(my_list):
+    for k,l in enumerate(my_list):
+    # print(j,my_list[i])
+        if j[0] == my_list[k][0] and j[1] == my_list[k][1]:
+            continue
+        elif j[0] < my_list[k][0] and j[1] < my_list[k][1]:
+            my_rank += 1
+    # print(my_rank)
+    my_rank_list.append(my_rank)
+    my_rank = 1
+print(*my_rank_list)
+
+
+
+# 이중루프만들지말고 딕셔너리로 구현할까? 그럼 시간복잡도 n 일텐데
+>>>>>>> 363a7414a868f5273eb888715af9da62ae8902eb
