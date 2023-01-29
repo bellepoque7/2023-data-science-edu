d13b31f5e37951b0c8d58638becc82617c448ef0
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-01-30 01:21:27 +0900

    11
    11

--- 1912 연속합.py
+++ 1912 연속합.py
@@ -4,21 +4,22 @@ def getSubsum(data) :
     '''
     n개의 숫자가 list로 주어질 때, 그 연속 부분 최대합을 반환하는 함수를 작성하세요.
     '''
-    sum = 0
-    res = -1e9
-    for i in data:
-        sum += i
-        if sum < 0:
-            sum = 0
-            continue
-        res = max(res,sum)
-    return res
+    my_sum = []
+    n = int(input())
+    n = len(data)
+    for i in range(2,n+1): #2부터 8까지 반복
+        for j in range(0,n-i+1):
+            if j == n-i:
+                my_sum.append(sum(data[j:]))
+            else:
+                my_sum.append(sum(data[j:j+i])) 
+    # print(my_sum)
+    return max(my_sum)
 
 def main():
     '''
     이 부분은 수정하지 마세요.
     '''
-    n = int(input())
     data = [int(x) for x in input().split()]
 
     print(getSubsum(data))
