2f42fe7a413051440fba94bf82a07d1101800825
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-01-30 13:53:06 +0900

    11
    11

--- 03-03 분할정복/2630 색종이 만들기.py
+++ 03-03 분할정복/2630 색종이 만들기.py
@@ -3,17 +3,21 @@ import sys
 N = int(sys.stdin.readline())
 paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
 
+# print(paper)  # 2D array 
 result = []
 
-def solution(x, y, N) :
-  color = paper[x][y]
-  for i in range(x, x+N) :
-    for j in range(y, y+N) :
+def solution(r, c, N) :
+  #x,y = 0,0의 value에 대해서 color에 할당
+  color = paper[r][c]
+  # 원점부터 끝까지 탐색.
+  for i in range(r, r+N) :
+    #열도 탐색
+    for j in range(c, c+N) :
       if color != paper[i][j] :
-        solution(x, y, N//2)
-        solution(x, y+N//2, N//2)
-        solution(x+N//2, y, N//2)
-        solution(x+N//2, y+N//2, N//2)
+        solution(r, c, N//2)
+        solution(r, c+N//2, N//2)
+        solution(r+N//2, c, N//2)
+        solution(r+N//2, c+N//2, N//2)
         return
   if color == 0 :
     result.append(0)
