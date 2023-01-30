3366e0b75ebe8e2240f2d6704bf997a7c95c24ec
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-01-30 15:42:03 +0900

    11
    11

--- 03-03 분할정복/쿼드트리_진홍석.py
+++ 03-03 분할정복/쿼드트리_진홍석.py
@@ -1,41 +1,5 @@
-# import sys
-# read=sys.stdin.readline
-# n=int(read())
-# li=[]
-# for i in range(n):
-#   c=list(map(int,read().strip()))
-#   li.append(c)
-# # print(li)
-
-# def cal(r,c,x):
-#   cnt=0
-#   # if r == 0 and c == 4 and x ==2:
-#   #   print('check')
-#   for i in range(c,c+x):
-#     for j in range(r,r+x):
-#       if li[i][j]==1:
-#         cnt+=1
-#   if cnt == x*x:
-#     return '1'
-#   if cnt == 0:
-#     return '0'
-#         # pass
-#     # if r == 0 and c == 4 and x ==2:
-#     #   print('check2')
-    
-#   cnt=0
-#   dc=[c,c,c+x//2,c+x//2] #왼위 오위 왼아 오아 
-#   dr=[r,r+x//2,r,r+x//2]
-  
-#   res=''
-#   for i in range(4):
-#     res+=cal(dr[i],dc[i],x//2)
-#   return '(' + res +')'
-
-# print(cal(0,0,n))
-
-
-
+#문제
+# 재귀에서 전달하는 파리미터이 r,c 값이 바뀌었었음 
 
 import sys
 read=sys.stdin.readline
@@ -44,12 +8,12 @@ li=[]
 for i in range(n):
   c=list(map(int,read().strip()))
   li.append(c)
-print(li)
+# print(li)
 
 def cal(r,c,x):
   cnt=0
   # if r == 0 and c == 4 and x ==2:
-    # print('check')
+  #   print('check')
   for i in range(c,c+x):
     for j in range(r,r+x):
       if li[i][j]==1:
@@ -63,12 +27,12 @@ def cal(r,c,x):
     #   print('check2')
     
   cnt=0
+  dc=[c,c,c+x//2,c+x//2] #왼위 오위 왼아 오아 
+  dr=[r,r+x//2,r,r+x//2]
   
-  dr=[r,r,r+x//2,r+x//2]
-  dc=[c,c+x//2,c,c+x//2]
   res=''
   for i in range(4):
-    res+=cal(dc[i],dr[i],x//2)
+    res+=cal(dr[i],dc[i],x//2)
   return '(' + res +')'
 
 print(cal(0,0,n))
