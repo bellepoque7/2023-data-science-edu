916e9e8e1691456db41aa500cfd219a7e2b44562
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-01-31 17:17:54 +0900

    11
    11

--- 03-04 그리디 알고리즘/14469번 - 소가 길을 건너간 이유 3.py
+++ 03-04 그리디 알고리즘/14469번 - 소가 길을 건너간 이유 3.py
@@ -1,7 +1,6 @@
 import sys
 read = sys.stdin.readline
 
-
 def main():
     N = int(read().rstrip())
     a = []
@@ -11,16 +10,16 @@ def main():
 
     # print(a) #[(2,1),(8,3),(5,7)]
     
-    # 시작시간 기준 정렬
+    # 시작시간 기준 정렬  최대로 우겨넣어서 통과하는게아닌. 그냥 전체종료시간만 구하면됨.
     a.sort()    #[(2,1),(5,7),(8,3)]
     # 첫 번째 소 도착 시간으로 세팅
-    res = a[0][0]
+    end = a[0][0]
 
     for cow in a:
-        res = max(cow[0], res)  # 현재 시간이 도착 시간보다 늦으면 그대로 사용하고 빠르면 도착 시간으로 변경
-        res += cow[1]   # 검문 시간 만큼 더하기
+        end = max(cow[0], end)  # 현재 시간이 도착 시간보다 늦으면 그대로 사용하고 빠르면 도착 시간으로 변경
+        end += cow[1]   # 검문 시간 만큼 더하기
 
-    print(res)
+    print(end)
 
 
 if __name__ == "__main__":
