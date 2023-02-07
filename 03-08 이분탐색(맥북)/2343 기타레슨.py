74eb4a222887aa3cbb48c60f886db8cff2aa4c5b
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-02-07 16:27:22 +0900

    11
    11

--- 03-08 이분탐색(맥북)/2343 기타레슨.py
+++ 03-08 이분탐색(맥북)/2343 기타레슨.py
@@ -7,14 +7,22 @@ n, m = map(int, input().split())
 
 video = list(map(int, input().split()))
 
+#vm에 강의 길이값의 최대를 넣음
 vm = max(video)
+#시작 위치 초기화
 start = vm
+#종료위치는 모든 강의를 1개 블루레이에 넣는 경우
 end = sum(video)
 
+#결과값 초기화
 res = 10**9
+
+#이진탐색 시작
 while(start <= end):
     mid = (start+end)//2
+    #최소 1개의 블루레이는 필요함
     cnt = 1
+    #블루레이의 길이
     tmp = 0
     for i in range(n):
         if(tmp+video[i] <= mid):
