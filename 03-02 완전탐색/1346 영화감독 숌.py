eef4ebb2ce7a91b3fdd2fbf76127480576520a09
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-02-20 17:31:12 +0900

    11
    11

--- 03-02 완전탐색/1346 영화감독 숌.py
+++ 03-02 완전탐색/1346 영화감독 숌.py
@@ -1,14 +1,15 @@
 n = int(input())
 movie = 666
 
+#O(n) 만큼 수행되는데, n=10,000  타임아웃안남.
+
 while True:
-    movie += 1
     if n == 0:
         break
 
     if "666" in str(movie):
         n -= 1
-    
+    movie += 1
     # print(movie)
 
 print(movie - 1)
