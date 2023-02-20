534d794c25292014895c266c72c6a443fa426ce2
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-02-20 16:40:30 +0900

    11
    11

---
+++ 03-02 완전탐색/1346 영화감독 숌.py
@@ -0,0 +1,15 @@
+n = int(input())
+movie = 666
+
+while True:
+    movie += 1
+    if n == 0:
+        break
+
+    if "666" in str(movie):
+        n -= 1
+    
+    # print(movie)
+
+print(movie - 1)
+
